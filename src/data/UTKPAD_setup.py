aa4ec49868ad4604ce3b387f3c2ca35364aee14e
n24q02m <quangminh2422004@gmail.com>
2024-11-27 14:17:14 +0700

    feat: Remove public flag from Kaggle dataset creation in UTKPAD setup script

--- src/data/UTKPAD_setup.py
+++ src/data/UTKPAD_setup.py
@@ -96,7 +96,6 @@ def upload_to_kaggle():
     print('Creating dataset on Kaggle...')
     api.dataset_create_new(
         folder=dataset_dir,
-        public=True,
         dir_mode='zip',
         quiet=False
     )
